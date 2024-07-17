908256657624f63ca532f215f3eac29bf860abc0
parkhyunseo <dazzle1999@naver.com>
2024-07-17 17:15:23 +0900

    Feat: 쿠폰 생성 Test Code 추가(#2)

---
+++ src/test/java/com/T82/coupon/service/CouponServiceImplTest.java
@@ -0,0 +1,43 @@
+package com.T82.coupon.service;
+
+import com.T82.coupon.dto.request.CreateCouponRequestDto;
+import com.T82.coupon.global.domain.enums.Category;
+import com.T82.coupon.global.domain.enums.DiscountType;
+import com.T82.coupon.global.domain.repository.CouponRepository;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class CouponServiceImplTest {
+    @Autowired
+    CouponServiceImpl couponService;
+
+    @Autowired
+    CouponRepository couponRepository;
+
+    @Nested
+    @Transactional
+    class 쿠폰생성 {
+        @Test
+        void 쿠폰생성_성공() {
+//    given
+            CreateCouponRequestDto coupon = new CreateCouponRequestDto("테스트쿠폰", DiscountType.FIXED, 1000, Date.from(Instant.parse("2024-12-31T23:59:59.00Z.")) , 10000, true, Category.SPORTS);
+            int lengthBefore = couponRepository.findAll().size();
+//    when
+            couponService.createCoupon(coupon);
+//    then
+            assertEquals(couponRepository.findAll().size(),lengthBefore + 1);
+        }
+
+    }
+}
