d498f4d2de6d0041fc5704c61c1e529f58359865
parkhyunseo <dazzle1999@naver.com>
2024-08-04 03:52:44 +0900

    Feat: CouponEvent 엔티티 생성 및 연관관계 매핑

---
+++ src/main/java/com/T82/coupon/global/domain/entity/CouponEvent.java
@@ -0,0 +1,33 @@
+package com.T82.coupon.global.domain.entity;
+
+import com.T82.coupon.global.domain.enums.Category;
+import com.T82.coupon.global.domain.enums.DiscountType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "COUPON_EVENTS")
+public class CouponEvent {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "COUPON_EVENT_ID")
+    private String couponEventId;
+    @Column(name = "TOTAL_COUPON")
+    private int totalCoupon;
+    @Column(name = "REST_COUPON")
+    private int restCoupon;
+    @Column(name = "EVENT_START_TIME")
+    private Date eventStartTime;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "COUPON_ID")
+    private Coupon coupon;
+}
