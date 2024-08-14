897c6b8aa69cc668000bee4815c7a0258dea2e5a
parkhyunseo <dazzle1999@naver.com>
2024-08-14 17:16:54 +0900

    Feat: 만보기 쿠폰 반환

--- src/main/java/com/T82/coupon/global/domain/repository/CouponRepository.java
+++ src/main/java/com/T82/coupon/global/domain/repository/CouponRepository.java
@@ -2,8 +2,6 @@ package com.T82.coupon.global.domain.repository;
 
 import com.T82.coupon.global.domain.entity.Coupon;
 import com.T82.coupon.global.domain.enums.Category;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,7 +10,7 @@ import java.util.List;
 import java.util.UUID;
 
 public interface CouponRepository extends JpaRepository<Coupon, UUID> {
-    @Query("select c from Coupon c where c.category=:category and c.validEnd > current_date")
+    @Query("select c from Coupon c where c.category=:category or c.category=com.T82.coupon.global.domain.enums.Category.PEDOMETER and c.validEnd > current_date")
     List<Coupon> findAllByCategory(@Param("category") Category category);
 
     @Query("select distinct ce.coupon from CouponEvent ce where  ce.eventStartTime < current_date")
